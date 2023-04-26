73539109fed1b7c17ac44c2b09fedbde482b3f78
petrkanapelka <kanapelka-petr@mail.ru>
2023-04-26 21:53:42 +0300

    feat: create function createButton and add json files

--- js/script.js
+++ js/script.js
@@ -5,7 +5,38 @@ function createTextarea() {
   textArea.setAttribute('cols', 80);
   textArea.setAttribute('rows', 5);
   textArea.classList.add('textarea');
-  //  const button = document.createElement('BUTTON');
   document.querySelector('body').appendChild(textArea);
 }
 createTextarea();
+
+function createKeyBoard() {
+  const keyBoard = document.createElement('div');
+  keyBoard.classList.add('keyboard');
+  document.querySelector('body').appendChild(keyBoard);
+}
+createKeyBoard();
+
+function createButton(btn) {
+  const button = document.createElement('div');
+  button.textContent = btn.key;
+  button.classList.add('key');
+  if (btn.key === 'Backspace' || btn.key === 'Tab' || btn.key === 'Delete') {
+    button.classList.add('key_black');
+  }
+  const newLine = document.querySelectorAll('.row_new');
+  newLine[newLine.length - 1].appendChild(button);
+}
+const rowOne = 'json/row1.json';
+const rowTwo = 'json/row2.json';
+async function getKeys(row) {
+  const res = await fetch(row);
+  const data = await res.json();
+  const newRow = document.createElement('div');
+  newRow.classList.add('row_new');
+  document.querySelector('.keyboard').appendChild(newRow);
+  data.forEach((button) => {
+    createButton(button);
+  });
+}
+getKeys(rowOne);
+getKeys(rowTwo);
